ab3512e3cade5bc3cee45d619a4588349250c51c
Khanh Dinh <dinhnguyenkhanh210401@gmail.com>
2021-09-25 15:24:47 +0700

    featured_detail_page

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -20,7 +20,9 @@
                     </div>
 
                     <div class="books-item-info">
-                        <h5 class="books-title">{{ $product->name }}</h5>
+                        <a href="{{ route('product.single', ['product' => $product->id]) }}">
+                            <h5 class="books-title">{{ $product->name }}</h5>
+                        </a>
 
                         <div class="books-price">{{ $product->price }}</div>
                     </div>
@@ -41,26 +43,6 @@
 
             <div class="col-lg-12">{{ $products->links('vendor.pagination.default') }}</div>
 
-            <div class="col-lg-12">
-
-                <nav class="navigation align-center">
-
-                    <a href="#" class="page-numbers bg-border-color current"><span>1</span></a>
-                    <a href="#" class="page-numbers bg-border-color"><span>2</span></a>
-                    <a href="#" class="page-numbers bg-border-color"><span>3</span></a>
-                    <a href="#" class="page-numbers bg-border-color"><span>4</span></a>
-                    <a href="#" class="page-numbers bg-border-color"><span>5</span></a>
-
-                    <svg class="btn-prev">
-                        <use xlink:href="#arrow-left"></use>
-                    </svg>
-                    <svg class="btn-next">
-                        <use xlink:href="#arrow-right"></use>
-                    </svg>
-
-                </nav>
-
-            </div>
 
         </div>
     </div>
